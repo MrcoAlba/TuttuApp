0851b8a81192e735d6c7f8ac05a8dd913aa3d56e
Mrco Alba <mrcoalba@gmail.com>
2025-11-12 17:32:19 -0500

    Initial Commit

---
+++ Tuttu/TuttuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuttuApp.swift
+//  Tuttu
+//
+//  Created by Marco Antonio Landauro Alba on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TuttuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
